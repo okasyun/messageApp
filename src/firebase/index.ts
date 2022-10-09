ffb98f672e5f936e6b3344bc3940d677521dcda5
okasyun <shunshun1207shun@keio.jp>
2022-10-09 19:20:05 +0900

    first commit

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { firebaseConfig } from "./config";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+};
 
 const firebase = initializeApp(firebaseConfig);
 
